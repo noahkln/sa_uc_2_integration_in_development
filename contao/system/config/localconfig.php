c60dc4cae0274708f4e4802ce982e6ef8b2df518
Noah Hartwig <noah.hartwig@addmore.de>
2022-12-12 15:39:40 +0100

    Finished setup with dolt and contao

--- contao/system/config/localconfig.php
+++ contao/system/config/localconfig.php
@@ -2,5 +2,5 @@
 
 ### INSTALL SCRIPT START ###
 $GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
-$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$uW2H42tEBfh85fqURsl1uuHmS2HMTNa57OFSWhzogVTtmfz8n1Vx.';
+$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$vyEMNHkx.robWKkIKguCX.59YN9EgHA.YY7E5jDxPzuiO9i7g9xsW';
 ### INSTALL SCRIPT STOP ###
